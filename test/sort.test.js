0209f49d53d4a6d5c544ac4bf481597d9e732701
julio-crypt0 <hernandezjulio58@ymail.com>
2019-10-30 08:55:12 -0700

    sort test

---
+++ test/sort.test.js
@@ -0,0 +1,15 @@
+const sort = require('../sort');
+const { expect } = require('chai');
+
+describe('Sort Function', () => {
+  it('sort function sorts properly', () => {
+    const actual = sort([5, 4, 3, 2, 1]);
+    const correct = [1, 2, 3, 4, 5];
+    expect(actual).to.deep.equal(correct);
+  });
+  it('playground', () => {
+    const a = { x: 5 };
+    const b = { x: 5 };
+    expect(a).to.deep.equal(b);
+  });
+});
