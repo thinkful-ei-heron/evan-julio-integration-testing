4a5cf2276fea03c75e789525bb76e81ebb6f65e1
Evan Vogts <evanjvogts@gmail.com>
2019-10-30 10:36:58 -0500

    mocha

---
+++ test/divide.test.js
@@ -0,0 +1,20 @@
+const divide = require('../index');
+const { expect } = require('chai');
+
+describe('Divide function', () => {
+  it('should divide positive integers correctly', () => {
+    const a = 8;
+    const b = 4;
+    const expectedAnswer = 2;
+    const actualAnswer = divide(a, b);
+    expect(actualAnswer).to.equal(expectedAnswer);
+  });
+
+  it('should throw an error when divide by zero', () => {
+    const a = 8, b = 0;
+    const fn = () => {
+      divide(a,b)
+    };
+    expect(fn).to.throw();
+  });
+});
